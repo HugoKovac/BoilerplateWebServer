f2e289771fdfc4a40571b7b0594da653729b3a37
Hugo Kovac <hugokovac@Hugos-Air.fritz.box>
2023-12-18 19:08:46 +0100

    add port to to dev nginx

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
 		server: {
 			proxy: {
 				'/dev': {
-					target: 'http://localhost:3000',
+					target: 'http://web_dev:3000',
 					changeOrigin: true,
 					rewrite: (path) => path.replace(/^\/dev/, ''),
 				},
