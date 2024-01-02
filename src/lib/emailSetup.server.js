0012628f94dab3089136ce4e646789f2055a2a4d
Hugo Kovac <hugokovac@Hugos-MacBook-Air.local>
2024-01-02 14:47:10 +0100

    remove google env var

--- src/lib/emailSetup.server.js
+++ src/lib/emailSetup.server.js
@@ -1,8 +1,6 @@
 import nodemailer from "nodemailer";
 import {SMTP_EMAIL, SMTP_PASSWORD, SMTP_PORT, SMTP_SERVER} from '$env/static/private'
 
-console.log(GOOGLE_PASSWORD);
-
 let transporter = nodemailer.createTransport({
     host: SMTP_SERVER,
     port: SMTP_PORT,
