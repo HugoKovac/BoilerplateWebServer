7e7ae34c099522b9096a58051f034d9abc6f4bd4
hugokovac <kovmaxime@gmail.com>
2024-01-16 08:15:02 +0000

    Fix toast color and navbar for mobile

--- src/routes/verify/[slug]/+page.server.js
+++ src/routes/verify/[slug]/+page.server.js
@@ -4,7 +4,8 @@ import { validateEmailVerificationToken } from "$lib/server/token";
 import { auth } from "$lib/server/lucia";
 import { DEV } from "$env/static/private";
 
-export async function load({params, cookies}){
+// https://lucia-auth.com/guidebook/email-verification-links/sveltekit/#verify-email
+export async function load({params, cookies, locals}){
     const token = params.slug
     try {
 		const userId = await validateEmailVerificationToken(token);
@@ -18,13 +19,14 @@ export async function load({params, cookies}){
 			attributes: {}
 		});
 		const sessionCookie = auth.createSessionCookie(session);
-		cookies.set(sessionCookie.name, sessionCookie.value, {
-			path: '/',
-			httpOnly: true,
-			sameSite: 'strict',
-			secure: !DEV,
-			maxAge: 60 * 60 * 24 * 30
-		});
+		// cookies.set(sessionCookie.name, sessionCookie.value, {
+		// 	path: '/',
+		// 	httpOnly: true,
+		// 	sameSite: 'strict',
+		// 	secure: !DEV,
+		// 	maxAge: 60 * 60 * 24 * 30
+		// });
+		locals.auth.setSession(sessionCookie);
 	} catch (err) {
         console.error(err)
         throw redirect(303, "/login");
